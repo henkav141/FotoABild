a1e1c722ac6e0fb9877b7ed42e8ed19735649fc3
Johan Larsson <koslewblizz@gmail.com>
2016-03-01 14:59:11 +0100

    Android Mainpage

--- FotoABIld/FotoABIld/FotoABIld.Droid/MainActivity.cs
+++ FotoABIld/FotoABIld/FotoABIld.Droid/MainActivity.cs
@@ -12,22 +12,17 @@ namespace FotoABIld.Droid
 	[Activity (Label = "FotoABIld.Droid", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+            Window.RequestFeature(WindowFeatures.NoTitle);
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+
 		}
 	}
 }
