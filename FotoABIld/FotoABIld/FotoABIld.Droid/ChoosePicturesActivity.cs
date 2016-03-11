b4403a773687e9c7bcde859ad562972cea098270
Johan Larsson <koslewblizz@gmail.com>
2016-03-11 21:41:47 +0100

    Skapade en gridview istället för scrollview

--- FotoABIld/FotoABIld/FotoABIld.Droid/ChoosePicturesActivity.cs
+++ FotoABIld/FotoABIld/FotoABIld.Droid/ChoosePicturesActivity.cs
@@ -25,9 +25,30 @@ namespace FotoABIld.Droid
             SetContentView(Resource.Layout.ChoosePictures);
 
             Button cancelButton = FindViewById<Button>(Resource.Id.CancelButton);
+            var gridview = FindViewById<GridView>(Resource.Id.gridview);
 
+            gridview.Adapter = new ImageAdapter(this);
+
+            gridview.ItemClick += gridview_ItemClick;
+
+            //NumberPicker nr1 = FindViewById<NumberPicker>(Resource.Id.numberPicker1);
+            //NumberPicker nr2 = FindViewById<NumberPicker>(Resource.Id.numberPicker2);
+
+            //nr2.MaxValue = 20;
+            //nr2.MinValue = 0;
+            //nr2.Value = 0;
+            //nr2.Selected = true;
+            //nr1.MaxValue = 20;
+            //nr1.MinValue = 0;
+            //nr1.Value = 0;
+            //nr1.Selected = false;
             cancelButton.Click += Cancelbutton_Click;
 
+        }
+
+        void gridview_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Toast.MakeText(this, e.Position.ToString(), ToastLength.Short).Show();
         }
 
         private void Cancelbutton_Click(object sender, EventArgs e)
@@ -41,4 +62,4 @@ namespace FotoABIld.Droid
 
 
     }
-    }
+}
