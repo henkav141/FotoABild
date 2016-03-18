8d8248c565cedd6c294563b3265a5fa1d05c4e1e
Johan Larsson <koslewblizz@gmail.com>
2016-03-18 12:47:28 +0100

    Fixade klasser för multiple image picker

--- FotoABIld/FotoABIld/FotoABIld.Droid/ChoosePicturesActivity.cs
+++ FotoABIld/FotoABIld/FotoABIld.Droid/ChoosePicturesActivity.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Android.Animation;
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Com.Nostra13.Universalimageloader.Cache.Memory.Impl;
+using Com.Nostra13.Universalimageloader.Core;
+using Com.Nostra13.Universalimageloader.Core.Assist;
 using Uri = Android.Net.Uri;
 
 namespace FotoABIld.Droid
@@ -17,7 +21,10 @@ namespace FotoABIld.Droid
     public class ChoosePicturesActivity : Activity
     {
         List<Android.Net.Uri> uriList = new List<Android.Net.Uri>();
-        
+        private GridView gridview;
+        private GalleryAdapter galleryAdapter;
+        private Button chooseButton;
+        private ImageLoader imageLoader;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -28,25 +35,48 @@ namespace FotoABIld.Droid
             // Create your application here
             SetContentView(Resource.Layout.ChoosePictures);
 
-            Button cancelButton = FindViewById<Button>(Resource.Id.CancelButton);
-            Button chooseButton = FindViewById<Button>(Resource.Id.ChoosePicturesButton);
 
 
-            cancelButton.Click += Cancelbutton_Click;
-            chooseButton.Click += chooseButton_Click;
+
              
 
 
         }
 
+        private void InitImageLoader()
+        {
+            DisplayImageOptions defaultOptions =
+                new DisplayImageOptions.Builder().CacheOnDisc()
+                    .ImageScaleType(ImageScaleType.ExactlyStretched)
+                    .BitmapConfig(Bitmap.Config.
+                        Rgb565).Build();
+            ImageLoaderConfiguration.Builder builder =
+                new ImageLoaderConfiguration.Builder(this).DefaultDisplayImageOptions(defaultOptions)
+                    .MemoryCache(new WeakMemoryCache()
+                    );
+
+            ImageLoaderConfiguration config = builder.Build();
+            imageLoader = ImageLoader.Instance;
+            imageLoader.Init(config);
+        }
+
+        private void Init()
+        {
+           var handler = new Handler();
+           gridview = gridview = FindViewById<GridView>(Resource.Id.gridview);
+            galleryAdapter = new GalleryAdapter(ApplicationContext, imageLoader);
+            gridview.Adapter = galleryAdapter;
+            Button cancelButton = FindViewById<Button>(Resource.Id.CancelButton);
+            chooseButton = FindViewById<Button>(Resource.Id.ChoosePicturesButton);
+            cancelButton.Click += Cancelbutton_Click;
+            chooseButton.Click += chooseButton_Click;   
+
+
+        }
 
         void chooseButton_Click(object sender, EventArgs e)
         {
-            Intent intent = new Intent(Intent.ActionPick);
-            intent.SetType("image/*");
-            intent.SetAction(Intent.ActionGetContent);
-            intent.PutExtra(Intent.ExtraAllowMultiple, true);
-            SetResult(Result.Ok,intent);
+            Intent intent = new Intent(Action.ActionPickMultiple);
             StartActivityForResult(intent, 0);
 
 
@@ -68,23 +98,43 @@ namespace FotoABIld.Droid
 
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
         {
-            if (requestCode == 0)
+            base.OnActivityResult(requestCode, resultCode, intent);
+
+            if (requestCode == 0 && resultCode == Result.Ok)
             {
-                if (resultCode == Result.Ok)
+                var all_path = intent.GetStringArrayExtra("all_path");
+                var dataT = new List<CustomGallery>();
+
+                foreach (string uri in all_path)
                 {
-                        ClipData clipData = intent.ClipData;
-                        for (int i = 0; i < intent.ClipData.ItemCount; i++)
-                        {
-                            var item = clipData.GetItemAt(i);
-                            var uri = item.Uri;
-                            uriList.Add(uri);
-                    }
-                        ImageAdapter imageAdapter = new ImageAdapter(this, uriList);
-                        var gridview = FindViewById<GridView>(Resource.Id.gridview);
-                        gridview.Adapter = imageAdapter;
-                        gridview.ItemClick += gridview_ItemClick;
+                    var item = new CustomGallery();
+                    item.SdCardPath = uri;
+                    dataT.Add(item);
                 }
+                galleryAdapter.AddAll(dataT);
+
             }
         }
+
+        //protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        //{
+        //    if (requestCode == 0)
+        //    {
+        //        if (resultCode == Result.Ok)
+        //        {
+        //                ClipData clipData = intent.ClipData;
+        //                for (int i = 0; i < intent.ClipData.ItemCount; i++)
+        //                {
+        //                    var item = clipData.GetItemAt(i);
+        //                    var uri = item.Uri;
+        //                    uriList.Add(uri);
+        //            }
+        //                ImageAdapter imageAdapter = new ImageAdapter(this, uriList);
+        //                gridview = FindViewById<GridView>(Resource.Id.gridview);
+        //                gridview.Adapter = imageAdapter;
+        //                gridview.ItemClick += gridview_ItemClick;
+        //        }
+        //    }
+        //}
     }
 }
