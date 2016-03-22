86771a594a2a437f7a78f050e17c12373f5ad4ca
Richard Strålin Norberg <tha_saint_8@hotmail.com>
2016-03-22 11:42:00 +0100

    FinalizeOrder

---
+++ FotoABIld/FotoABIld/FotoABIld.Droid/FinalizeActivity.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FotoABIld.Droid
+{
+    [Activity(Label = "FinalizeActivity")]
+    public class FinalizeActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Window.RequestFeature(WindowFeatures.NoTitle);
+
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.FinalizeOrder);
+
+            Button cancelButton = FindViewById<Button>(Resource.Id.CancelButton);
+
+            cancelButton.Click += CancelButton_Click;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            var info = new Intent(this, typeof(CustomerInformationActivity));
+            StartActivity(info);
+        }
+    }
+}
