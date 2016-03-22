d949c74a8731aeebf51b3a57a6afa14f3459bc00
Johan Larsson <koslewblizz@gmail.com>
2016-03-22 12:54:21 +0100

    Fixat spinner och numberpicker till editpicture

--- FotoABIld/FotoABIld/FotoABIld.Droid/EditPictureActivity.cs
+++ FotoABIld/FotoABIld/FotoABIld.Droid/EditPictureActivity.cs
@@ -22,28 +22,53 @@ namespace FotoABIld.Droid
 
 
     {
-        private ImageLoader imageLoader;
-
+        private ImageView imageView;
+        private NumberPicker numberPicker;
+        private Spinner spinner;
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Window.RequestFeature(WindowFeatures.NoTitle);
+
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.EditPicture);
            // Create your application here
 
+            Init();
+            InitNumberPicker();
+            InitSpinner();
             var position = Intent.GetStringExtra("single_path");
-            var imageView = FindViewById<ImageView>(Resource.Id.edit_picture);
+            
             File imgFile = new File(position);
             if (imgFile.Exists())
             {
                 Bitmap bitMap = BitmapFactory.DecodeFile(imgFile.AbsolutePath);
                 imageView.SetImageBitmap(bitMap);
             }
-            
-
-
-
-
         }
+
+        private void Init()
+        {
+            imageView = FindViewById<ImageView>(Resource.Id.edit_picture);
+            
+        }
+
+        private void InitNumberPicker()
+        {
+            numberPicker = FindViewById<NumberPicker>(Resource.Id.numberPicker1);
+            numberPicker.Value = 1;
+            numberPicker.MinValue = 0;
+            numberPicker.MaxValue = 100;
+        }
+
+        private void InitSpinner()
+        {
+            spinner = FindViewById<Spinner>(Resource.Id.spinner);
+            var adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.Sizes, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerItem);
+            spinner.Adapter = adapter;
+        }
+
+
 
     }
 }
