ac51257df1da9c44ccec40dd5334d2e3ceba15c3
Johan Larsson <koslewblizz@gmail.com>
2016-04-18 14:20:09 +0200

    Fixed FinalizeActivity

--- FotoABIld/FotoABIld/FotoABIld.Droid/FinalizeActivity.cs
+++ FotoABIld/FotoABIld/FotoABIld.Droid/FinalizeActivity.cs
@@ -33,7 +33,8 @@ namespace FotoABIld.Droid
             SetContentView(Resource.Layout.FinalizeOrder);
 
             Init();
-            AddTableRow();
+            var priceClass = new PriceClass(order.Pictures);
+            AddTableRow(priceClass);
         }
 
         private void Init()
@@ -52,79 +53,147 @@ namespace FotoABIld.Droid
             cancelButton.Click += CancelButton_Click;
         }
 
-        //Skapar TableRows för varje unik storlek som finns i ordern och lägger till antal och pris för varje.
-        //Fungerar ej till 100% beroende på prisrabatter ännu
-        private void AddTableRow()
+        private void AddTableRow(PriceClass priceClass)
         {
-            var differentSizes = (order.Pictures.Select(picture => picture.Size).Distinct().ToList());
-            differentSizes = differentSizes.OrderBy(size => size).ToList();
-            
-            var tableLayout = FindViewById<TableLayout>(Resource.Id.amountTable);
-            var tablerowList = new List<TableRow>();
             var amountHandler = new AmountHandler(order.Pictures);
-            foreach (var size in differentSizes)
+
+            var lPWeight1 = new LinearLayout.LayoutParams(0,
+                ViewGroup.LayoutParams.WrapContent, 1);
+
+            var lPWeight2 = new LinearLayout.LayoutParams(0,
+                ViewGroup.LayoutParams.WrapContent, 2);
+
+            var lpNoWeight = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent,
+                ViewGroup.LayoutParams.WrapContent);
+            var lPHorizontalWeight1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent,1);
+            var lPHorizontalWeight2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent, 2);
+            var lPPrice = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MatchParent, 1);
+
+            var summarizeLayout = FindViewById<LinearLayout>(Resource.Id.summarizePictures);
+            foreach (List<Pictures> picturelist in priceClass.PriceClasses)
             {
-                var tableRow = new TableRow(this);
+                var horizontalLayout = new LinearLayout(this)
+                {
+                    LayoutParameters = lpNoWeight,
+                    Orientation = Orientation.Horizontal,
+                };
                 
-                var layoutParameters = new TableRow.LayoutParams(TableRow.LayoutParams.WrapContent, TableRow.LayoutParams.MatchParent, 1);
-                var layoutParameters2 = new TableRow.LayoutParams(TableRow.LayoutParams.WrapContent, TableRow.LayoutParams.MatchParent, 2);
-
-
-                var sizeText = new TextView(this)
+                var sizeLayout = new LinearLayout(this)
                 {
-                    LayoutParameters = layoutParameters,
-                    Text = size,
-                    Gravity = GravityFlags.Left,
-                    TextSize = 20
-
+                    LayoutParameters = lPWeight1,
+                    Orientation = Orientation.Vertical
                 };
-                var amountText = new TextView(this)
+                sizeLayout.SetBackgroundColor(Color.YellowGreen);
+                var amountLayout = new LinearLayout(this)
                 {
-                    LayoutParameters = layoutParameters2,
-                    Text = amountHandler.GetAmountofSize(size).ToString(),
-                    Gravity = GravityFlags.Right,
-                    TextSize = 20
+                    LayoutParameters = lPWeight2,
+                    Orientation = Orientation.Vertical
                 };
+                amountLayout.SetBackgroundColor(Color.IndianRed);
 
+                var differentSizes = picturelist.Select(picture => picture.Size).Distinct().ToList();
+                differentSizes = differentSizes.OrderBy(size => size).ToList();
+
+                summarizeLayout.AddView(horizontalLayout);
+                horizontalLayout.AddView(sizeLayout);
+                horizontalLayout.AddView(amountLayout);
+                foreach (var size in differentSizes)
+                {
+                    var sizeText = new TextView(this)
+                    {
+                        LayoutParameters = lPHorizontalWeight1,
+                        Text = size,
+                        Gravity = GravityFlags.Left,
+                        TextSize = 20
+
+                    };
+                    sizeText.SetTextColor(Color.Black);
+                    var amountText = new TextView(this)
+                    {
+                        LayoutParameters = lPHorizontalWeight2,
+                        Text = amountHandler.GetAmountofSize(size).ToString(),
+                        Gravity = GravityFlags.CenterHorizontal,
+                        TextSize = 20
+                    };
+                    amountText.SetTextColor(Color.Black);
+                    sizeLayout.AddView(sizeText);
+                    amountLayout.AddView(amountText);
+                }
+
+                if (picturelist == null && picturelist.Count <= 0) continue;
                 var priceText = new TextView(this)
                 {
-                    LayoutParameters = layoutParameters,
-                    Text = PriceHandler.GetPriceOfSize(size, order.Pictures).ToString(),
+                    LayoutParameters = lPPrice,
                     Gravity = GravityFlags.Right,
                     TextSize = 20
                 };
-                if (differentSizes.Contains("10x15") && differentSizes.Contains("11x15") && size.Equals("10x15"))
-                {
-                    var amount = amountHandler.GetAmountofSize(size) + amountHandler.GetAmountofSize("11x15");
-
-                    priceText.Text = PriceCalculator.CalculatePrice("10x15", amount).ToString();
-
-                }
-                if (differentSizes.Contains("10x15") && differentSizes.Contains("11x15") && size.Equals("11x15"))
-                    priceText.Text = "";
-
-
-                sizeText.SetTextColor(Color.Black);
-                amountText.SetTextColor(Color.Black);
+                var amount = differentSizes.Sum(size => amountHandler.GetAmountofSize(size));
+                if(differentSizes.Count > 0)
+                priceText.Text = PriceCalculator.CalculatePrice(differentSizes[0],amount).ToString();
                 priceText.SetTextColor(Color.Black);
-                tableRow.AddView(sizeText);
-                tableRow.AddView(amountText);
-                tableRow.AddView(priceText);
-
-
-
-                tableLayout.AddView(tableRow);
+                horizontalLayout.AddView(priceText);
             }
-            var layoutParameters3 = new TableRow.LayoutParams(TableRow.LayoutParams.MatchParent, TableRow.LayoutParams.MatchParent, 1);
-
-
-            foreach (var tableRow in tablerowList)
-            {
-                tableLayout.AddView(tableRow);
-            }
-
-
         }
+        //private void AddTableRow()
+        //{
+        //    var differentSizes = (order.Pictures.Select(picture => picture.Size).Distinct().ToList());
+        //    differentSizes = differentSizes.OrderBy(size => size).ToList();
+            
+        //    var tableLayout = FindViewById<TableLayout>(Resource.Id.summarizePictures);
+        //    var amountHandler = new AmountHandler(order.Pictures);
+        //    foreach (var size in differentSizes)
+        //    {
+        //        var tableRow = new TableRow(this);
+                
+        //        var layoutParameters = new TableRow.LayoutParams(TableRow.LayoutParams.WrapContent, TableRow.LayoutParams.MatchParent, 1);
+        //        var layoutParameters2 = new TableRow.LayoutParams(TableRow.LayoutParams.WrapContent, TableRow.LayoutParams.MatchParent, 2);
+
+
+        //        var sizeText = new TextView(this)
+        //        {
+        //            LayoutParameters = layoutParameters,
+        //            Text = size,
+        //            Gravity = GravityFlags.Left,
+        //            TextSize = 20
+
+        //        };
+        //        var amountText = new TextView(this)
+        //        {
+        //            LayoutParameters = layoutParameters2,
+        //            Text = amountHandler.GetAmountofSize(size).ToString(),
+        //            Gravity = GravityFlags.Right,
+        //            TextSize = 20
+        //        };
+
+        //        var priceText = new TextView(this)
+        //        {
+        //            LayoutParameters = layoutParameters,
+        //            Text = PriceHandler.GetPriceOfSize(size, order.Pictures).ToString(),
+        //            Gravity = GravityFlags.Right,
+        //            TextSize = 20
+        //        };
+        //        if (differentSizes.Contains("10x15") && differentSizes.Contains("11x15") && size.Equals("10x15"))
+        //        {
+        //            var amount = amountHandler.GetAmountofSize(size) + amountHandler.GetAmountofSize("11x15");
+
+        //            priceText.Text = PriceCalculator.CalculatePrice("10x15", amount).ToString();
+
+        //        }
+        //        if (differentSizes.Contains("10x15") && differentSizes.Contains("11x15") && size.Equals("11x15"))
+        //            priceText.Text = "";
+
+
+
+        //        tableRow.AddView(sizeText);
+        //        tableRow.AddView(amountText);
+        //        tableRow.AddView(priceText);
+
+
+
+        //        tableLayout.AddView(tableRow);
+        //    }
+
+        //}
 
         private void CancelButton_Click(object sender, EventArgs e)
         {
---
+++ FotoABIld/FotoABIld/FotoABIld/PriceClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace FotoABIld
+{
+    public  class PriceClass
+    {
+        private List<Pictures> SmallPictures { get; set; }
+        private List<Pictures> MediumSmallPictures { get; set; }
+        private List<Pictures> MediumLargePictures { get; set; }
+        private List<Pictures> LargePictures { get; set; }
+        public  List<List<Pictures>> PriceClasses { get; set; }
+
+
+        public PriceClass(List<Pictures> pictureList)
+        {
+            PriceClasses = new List<List<Pictures>>();
+            SmallPictures =
+                pictureList.Where(picture => picture.Size.Equals("10x15") || picture.Size.Equals("11x15")).ToList();
+            MediumSmallPictures = 
+                pictureList.Where(picture => picture.Size.Equals("13x18(vit kant)") || picture.Size.Equals("15x21")).ToList();
+            MediumLargePictures = 
+                pictureList.Where(picture => picture.Size.Equals("18x24(vit kant)") || picture.Size.Equals("20x30")).ToList();
+            LargePictures = 
+                pictureList.Where(picture => picture.Size.Equals("24x30(vit kant)") || picture.Size.Equals("25x38")).ToList();
+            if (SmallPictures != null)
+            {
+                PriceClasses.Add(SmallPictures);
+            }
+            if (MediumSmallPictures != null)
+            {
+                PriceClasses.Add(MediumSmallPictures);
+            }
+            if (MediumLargePictures != null)
+            {
+                PriceClasses.Add(MediumLargePictures);
+            }
+            if (LargePictures != null)
+            {
+                PriceClasses.Add(LargePictures);
+            }
+        }
+
+        public int GetPriceByClass( )
+        {
+            return 0;
+        }
+    }
+}
