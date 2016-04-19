067938c92734a026bd3100c83c397905be96b5ba
Johan Larsson <koslewblizz@gmail.com>
2016-04-19 11:22:35 +0200

    Completed finalizing order

--- FotoABIld/FotoABIld/FotoABIld.Droid/Activities/FinalizeActivity.cs
+++ FotoABIld/FotoABIld/FotoABIld.Droid/Activities/FinalizeActivity.cs
@@ -56,40 +56,30 @@ namespace FotoABIld.Droid
         private void AddTableRow(PriceClass priceClass)
         {
             var amountHandler = new AmountHandler(order.Pictures);
-
             var lPWeight1 = new LinearLayout.LayoutParams(0,
                 ViewGroup.LayoutParams.WrapContent, 1);
-
             var lPWeight2 = new LinearLayout.LayoutParams(0,
                 ViewGroup.LayoutParams.WrapContent, 2);
-
             var lpNoWeight = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent,
                 ViewGroup.LayoutParams.WrapContent);
-            var lPHorizontalWeight1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent,1);
-            var lPHorizontalWeight2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent, 2);
+            var lPHorizontalWeight1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, 
+                ViewGroup.LayoutParams.WrapContent,1);
+            var lPHorizontalWeight2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, 
+                ViewGroup.LayoutParams.WrapContent, 2);
             var lPPrice = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MatchParent, 1);
 
+
             var summarizeLayout = FindViewById<LinearLayout>(Resource.Id.summarizePictures);
+            summarizeLayout.AddView(CreateDivider());
             foreach (List<Pictures> picturelist in priceClass.PriceClasses)
             {
-                var horizontalLayout = new LinearLayout(this)
-                {
-                    LayoutParameters = lpNoWeight,
-                    Orientation = Orientation.Horizontal,
-                };
-                
-                var sizeLayout = new LinearLayout(this)
-                {
-                    LayoutParameters = lPWeight1,
-                    Orientation = Orientation.Vertical
-                };
-                sizeLayout.SetBackgroundColor(Color.YellowGreen);
-                var amountLayout = new LinearLayout(this)
-                {
-                    LayoutParameters = lPWeight2,
-                    Orientation = Orientation.Vertical
-                };
-                amountLayout.SetBackgroundColor(Color.IndianRed);
+                var horizontalLayout = CreateLinearLayout(lpNoWeight, Orientation.Horizontal);
+
+                var sizeLayout = CreateLinearLayout(lPWeight2, Orientation.Vertical);
+
+                var amountLayout = CreateLinearLayout(lPWeight1, Orientation.Vertical);
+
+
 
                 var differentSizes = picturelist.Select(picture => picture.Size).Distinct().ToList();
                 differentSizes = differentSizes.OrderBy(size => size).ToList();
@@ -99,54 +89,89 @@ namespace FotoABIld.Droid
                 horizontalLayout.AddView(amountLayout);
                 foreach (var size in differentSizes)
                 {
-                    var sizeText = new TextView(this)
-                    {
-                        LayoutParameters = lPHorizontalWeight1,
-                        Text = size,
-                        Gravity = GravityFlags.Left,
-                        TextSize = 20
-
-                    };
-                    sizeText.SetTextColor(Color.Black);
-                    var amountText = new TextView(this)
-                    {
-                        LayoutParameters = lPHorizontalWeight2,
-                        Text = amountHandler.GetAmountofSize(size).ToString(),
-                        Gravity = GravityFlags.CenterHorizontal,
-                        TextSize = 20
-                    };
-                    amountText.SetTextColor(Color.Black);
+                    var sizeText = CreateSizeTextView(lPHorizontalWeight2,size);
+                    sizeText.SetBackgroundColor(Color.NavajoWhite);
+                    var amountText = CreateAmountTextView(lPHorizontalWeight1,
+                        amountHandler.GetAmountofSize(size).ToString());
+                    amountText.SetBackgroundColor(Color.BlanchedAlmond);
                     sizeLayout.AddView(sizeText);
                     amountLayout.AddView(amountText);
                 }
 
                 if (picturelist == null && picturelist.Count <= 0) continue;
-                var priceText = new TextView(this)
-                {
-                    LayoutParameters = lPPrice,
-                    Gravity = GravityFlags.Bottom|GravityFlags.Right,
-                    
-                    TextSize = 20
-                };
+                
+
 
                 var amount = differentSizes.Sum(size => amountHandler.GetAmountofSize(size));
-                if(differentSizes.Count > 0)
-                priceText.Text = PriceCalculator.CalculatePrice(differentSizes[0],amount).ToString();
-                priceText.SetBackgroundColor(Color.Aqua);
-                priceText.SetTextColor(Color.Black);
-                horizontalLayout.AddView(priceText);
-                summarizeLayout.AddView(CreateDivider());
 
+                if (differentSizes.Count > 0)
+                {
+                    var price = PriceCalculator.CalculatePrice(differentSizes[0], amount) + " kr";
+                    var priceText = CreatePriceTextView(lPPrice,
+                    price);
+                    horizontalLayout.AddView(priceText);
+                }
+                summarizeLayout.AddView(CreateDivider());
             }
         }
 
+        private View CreateSizeTextView(ViewGroup.LayoutParams lP, string size)
+        {
+            var sizeText = new TextView(this)
+            {
+                LayoutParameters = lP,
+                Text = size,
+                Gravity = GravityFlags.Left,
+                TextSize = 20
+
+            };
+            sizeText.SetTextColor(Color.Black);
+            return sizeText;
+        } 
+
+        private View CreateAmountTextView(ViewGroup.LayoutParams lP, string amount)
+        {
+            var amountText = new TextView(this)
+            {
+                LayoutParameters = lP,
+                Text = amount,
+                Gravity = GravityFlags.CenterHorizontal,
+                TextSize = 20
+            };
+            amountText.SetTextColor(Color.Black);
+            return amountText;
+        }
+
+        private View CreatePriceTextView(ViewGroup.LayoutParams lP, string price)
+        {
+            var priceText = new TextView(this)
+            {
+                LayoutParameters = lP,
+                Gravity = GravityFlags.Bottom | GravityFlags.Right,
+
+                TextSize = 20
+            };
+            priceText.Text = price;
+            priceText.SetTextColor(Color.Black);
+            return priceText;
+        }
         private View CreateDivider()
         {
             var view = new View(this);
             view.SetBackgroundColor(Color.ParseColor("#1F2F40"));
-            view.LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent,10);
+            view.LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent,1);
 
             return view;
+        }
+
+        private LinearLayout CreateLinearLayout(ViewGroup.LayoutParams lP,Orientation orientation)
+        {
+            var layout = new LinearLayout(this)
+            {
+                LayoutParameters = lP,
+                Orientation = orientation
+            };
+            return layout;
         }
         //private void AddTableRow()
         //{
