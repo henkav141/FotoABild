ad1ef6fb6e524056842f202a867459315cd94efc
Johan Larsson <johan.a.larsson@hotmail.com>
2016-05-04 09:07:12 +0200

    TestCommit

---
+++ FotoABIld/FotoABIld/FotoABIld/LacHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FotoABIld
+{
+    public static class LacHandler
+    {
+        public static void CreateLacFile(Order order,string filepath)
+        {
+            var lacText = new List<string> {FillUserDetails(order), FillOrderDetails(order)};
+
+            File.WriteAllLines(filepath, lacText);
+        }
+
+        private  static string FillUserDetails(Order order)
+        {
+            return "[User Details]" + "\r\n" +
+                   "FirstName=" + order.Name + "\r\n" +
+                   "Email=" + order.Email + "\r\n" +
+                   "LastName=" + order.Surname + "\r\n" +
+                   "Phone=" + order.PhoneNumber + "\r\n";
+
+        }
+
+        private static string FillOrderDetails(Order order)
+        {
+            return "[OrderDetails]" + "\r\n" +
+                   "OrderReference=" + "" + "\r\n" +
+                   "orderid=" + order.OrderId + "\r\n" +
+                   "TotalPrice=" + PriceCalculator.CalculateTotalPrice(order.Pictures) + "\r\n" +
+                   "OrderDate=" + DateTime.Now.ToString("dd/MM/yy hh:mm") + "\r\n" +
+                   "ReturnDate=" + order.Date.ToString("dd/MM/yy hh:mm") + "\r\n";
+
+        }
+    }
+}
---
+++ FotoABIld/FotoABIld/FotoABIld/OrderHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FotoABIld
+{
+    public static class OrderHandler    
+    {
+        public static string CreateOrderId()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, 20)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        
+        }
+        public static  void CreateOutputFolder(string filepath)
+        {
+            Directory.CreateDirectory(filepath);
+        }
+
+        public static void FillOutPutFolder(Order order, string targetPath)
+        {
+
+            for (var index = 0; index < order.Pictures.Count; index++)
+            {
+                var picture = order.Pictures[index];
+                File.Copy(picture.FilePath, targetPath + "/" + index);
+
+
+            }
+        }
+    }
+
+}
