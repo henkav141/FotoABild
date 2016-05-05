02ba9079bccf2b0244a83b5b18b1411bc1cc5f20
Johan Larsson <koslewblizz@gmail.com>
2016-05-05 13:02:48 +0200

    Started on fixing DrawerNavigation for Main

--- FotoABIld/FotoABIld/FotoABIld.Droid/Resources/Resource.designer.cs
+++ FotoABIld/FotoABIld/FotoABIld.Droid/Resources/Resource.designer.cs
@@ -1607,8 +1607,8 @@ namespace FotoABIld.Droid
 			// aapt resource value: 0x7f0b00b3
 			public const int FlyOutMenu = 2131427507;
 			
-			// aapt resource value: 0x7f0b008b
-			public const int FlyOutMenuButton = 2131427467;
+			// aapt resource value: 0x7f0b0089
+			public const int FlyOutMenuButton = 2131427465;
 			
 			// aapt resource value: 0x7f0b00b9
 			public const int HelpText = 2131427513;
@@ -1898,8 +1898,8 @@ namespace FotoABIld.Droid
 			// aapt resource value: 0x7f0b0084
 			public const int llBottomContainer = 2131427460;
 			
-			// aapt resource value: 0x7f0b008a
-			public const int mainLayout = 2131427466;
+			// aapt resource value: 0x7f0b008b
+			public const int mainLayout = 2131427467;
 			
 			// aapt resource value: 0x7f0b0092
 			public const int media_actions = 2131427474;
@@ -2114,8 +2114,8 @@ namespace FotoABIld.Droid
 			// aapt resource value: 0x7f0b0040
 			public const int textSpacerNoButtons = 2131427392;
 			
-			// aapt resource value: 0x7f0b0089
-			public const int textView1 = 2131427465;
+			// aapt resource value: 0x7f0b008a
+			public const int textView1 = 2131427466;
 			
 			// aapt resource value: 0x7f0b00b2
 			public const int thankText = 2131427506;
@@ -2291,79 +2291,82 @@ namespace FotoABIld.Droid
 			public const int gallery_item = 2130903072;
 			
 			// aapt resource value: 0x7f030021
-			public const int HelpPage = 2130903073;
+			public const int HamburgerMenu = 2130903073;
 			
 			// aapt resource value: 0x7f030022
-			public const int Main = 2130903074;
+			public const int HelpPage = 2130903074;
 			
 			// aapt resource value: 0x7f030023
-			public const int MainFrame = 2130903075;
+			public const int Main = 2130903075;
 			
 			// aapt resource value: 0x7f030024
-			public const int notification_media_action = 2130903076;
+			public const int MainFrame = 2130903076;
 			
 			// aapt resource value: 0x7f030025
-			public const int notification_media_cancel_action = 2130903077;
+			public const int notification_media_action = 2130903077;
 			
 			// aapt resource value: 0x7f030026
-			public const int notification_template_big_media = 2130903078;
+			public const int notification_media_cancel_action = 2130903078;
 			
 			// aapt resource value: 0x7f030027
-			public const int notification_template_big_media_narrow = 2130903079;
+			public const int notification_template_big_media = 2130903079;
 			
 			// aapt resource value: 0x7f030028
-			public const int notification_template_lines = 2130903080;
+			public const int notification_template_big_media_narrow = 2130903080;
 			
 			// aapt resource value: 0x7f030029
-			public const int notification_template_media = 2130903081;
+			public const int notification_template_lines = 2130903081;
 			
 			// aapt resource value: 0x7f03002a
-			public const int notification_template_part_chronometer = 2130903082;
+			public const int notification_template_media = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int notification_template_part_time = 2130903083;
+			public const int notification_template_part_chronometer = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int OrderHistory = 2130903084;
+			public const int notification_template_part_time = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int OrderHistoryItem = 2130903085;
+			public const int OrderHistory = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int OrderHistoryListItem = 2130903086;
+			public const int OrderHistoryItem = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int PricesSizes = 2130903087;
+			public const int OrderHistoryListItem = 2130903087;
 			
 			// aapt resource value: 0x7f030030
-			public const int Receipt = 2130903088;
+			public const int PricesSizes = 2130903088;
 			
 			// aapt resource value: 0x7f030031
-			public const int select_dialog_item_material = 2130903089;
+			public const int Receipt = 2130903089;
 			
 			// aapt resource value: 0x7f030032
-			public const int select_dialog_multichoice_material = 2130903090;
+			public const int select_dialog_item_material = 2130903090;
 			
 			// aapt resource value: 0x7f030033
-			public const int select_dialog_singlechoice_material = 2130903091;
+			public const int select_dialog_multichoice_material = 2130903091;
 			
 			// aapt resource value: 0x7f030034
-			public const int SideMenuLayout = 2130903092;
+			public const int select_dialog_singlechoice_material = 2130903092;
 			
 			// aapt resource value: 0x7f030035
-			public const int SpinnerItem = 2130903093;
+			public const int SideMenuLayout = 2130903093;
 			
 			// aapt resource value: 0x7f030036
-			public const int support_simple_spinner_dropdown_item = 2130903094;
+			public const int SpinnerItem = 2130903094;
 			
 			// aapt resource value: 0x7f030037
-			public const int titlebar = 2130903095;
+			public const int support_simple_spinner_dropdown_item = 2130903095;
 			
 			// aapt resource value: 0x7f030038
-			public const int ToolBar = 2130903096;
+			public const int titlebar = 2130903096;
 			
 			// aapt resource value: 0x7f030039
-			public const int TrashCheckBox = 2130903097;
+			public const int ToolBar = 2130903097;
+			
+			// aapt resource value: 0x7f03003a
+			public const int TrashCheckBox = 2130903098;
 			
 			static Layout()
 			{
