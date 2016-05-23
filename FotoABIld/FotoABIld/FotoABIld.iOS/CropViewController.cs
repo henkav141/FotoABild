0a89dec32314b12901e767073f9993c08a371633
henkav141 <henrik.karlsson.88@live.se>
2016-05-23 17:25:14 +0200

    iOS

--- FotoABIld/FotoABIld/FotoABIld.iOS/CropViewController.cs
+++ FotoABIld/FotoABIld/FotoABIld.iOS/CropViewController.cs
@@ -2,13 +2,15 @@ using Foundation;
 using System;
 using System.CodeDom.Compiler;
 using System.Drawing;
+using System.Linq;
 using UIKit;
 using CropBindingiOS;
-
+using FotoABIld.iOS.Controllers;
 
 
 namespace FotoABIld.iOS
 {
+    //Class to handle Cropper.
     partial class CropViewController : UIViewController
 	{
         private UIImage image;
@@ -21,8 +23,11 @@ namespace FotoABIld.iOS
 
         public override void ViewDidLoad()
         {
-            
-            image = UIImage.FromFile("elcapitan.jpg");
+            foreach (var z in ChooseImageController.ImageHandlerList.Where(z => z.Name.Equals(EditImageController.EditControllerName)))
+            {
+                image = z.Image;
+            }
+
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
         }
@@ -30,22 +35,23 @@ namespace FotoABIld.iOS
         public override void ViewDidAppear(bool animated)
         {
             base.ViewDidAppear(animated);
+            //Create and show the Crop View Controller.
             if (!IsShown)
             {
-
                 IsShown = true;
                 picker = new TOCropViewController(image);
                 PresentViewController(picker, false, null);
             }
-
-            picker.Toolbar.DoneTextButton.TouchUpInside += DoneTextButtonOnTouchUpInside;
+            
+            //picker.Toolbar.DoneTextButton.TouchUpInside += DoneTextButtonOnTouchUpInside;
+            picker.Toolbar.DoneIconButton.TouchUpInside += DoneTextButtonOnTouchUpInside;
         }
 
         private void DoneTextButtonOnTouchUpInside(object sender, EventArgs eventArgs)
         {
-
             imgV.Image = picker.Image.CroppedImageWithFrame(picker.CropView.CroppedImageFrame, picker.CropView.Angle);
         }
+
 
         public override void ViewWillDisappear(bool animated)
         {
