ba4f9fe16705a2a751d9e15fabcf0dcabc5820aa
Johan Larsson <johan.a.larsson@hotmail.com>
2016-07-07 12:17:19 +0200

    Removed ftphandler

--- FotoABIld/FotoABIld/FotoABIld/LacHandler.cs
+++ FotoABIld/FotoABIld/FotoABIld/LacHandler.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace FotoABIld
 {
-    public  class LacHandler
+    public class LacHandler
     {
         private Order order;
         private string folderfilepath;
@@ -18,15 +19,15 @@ namespace FotoABIld
             this.lacfilepath = lacfilepath;
             fileInfoList = new List<FileInfo>();
         }
-        public  void CreateLacFile()
+        public void CreateLacFile()
         {
             GetFileInfo();
-            var lacText = new List<string> {FillUserDetails(), FillOrderDetails(), FillFilesTransmitted(), FillFileSizes() };
-            
+            var lacText = new List<string> { FillUserDetails(), FillOrderDetails(), FillFilesTransmitted(), FillFileSizes(), FillFitOrFill(), Fill10X15() };
+
             File.WriteAllLines(lacfilepath, lacText);
         }
 
-        private   string FillUserDetails()
+        private string FillUserDetails()
         {
             return "[User Details]" + "\r\n" +
                    "FirstName=" + order.Name + "\r\n" +
@@ -36,7 +37,7 @@ namespace FotoABIld
 
         }
 
-        private  string FillOrderDetails()
+        private string FillOrderDetails()
         {
             return "[Order Details]\r\n" +
                    "OrderReference=\r\n" +
@@ -47,34 +48,75 @@ namespace FotoABIld
 
         }
 
-        private  string FillFilesTransmitted()
+        private string FillFilesTransmitted()
         {
             var transmittedString = "[Files Transmitted]\r\n";
-            for (int index = 0; index < fileInfoList.Count; index++)
+            //for (int index = 0; index < fileInfoList.Count; index++)
+            //{
+            //    var fileInfo = fileInfoList[index];
+            //    transmittedString += index + 1 + "=" + fileInfo.FullName + "\r\n";
+            //}
+            var index = 1;
+
+            foreach (var picture in order.Pictures)
             {
-                var fileInfo = fileInfoList[index];
-                transmittedString += index + 1 + "=" + fileInfo.FullName + "\r\n";
+                transmittedString += index + "=" + picture.FilePath + "\r\n";
+                index++;
             }
+
             return transmittedString;
         }
 
         private string FillFileSizes()
         {
             var sizeString = "[File Sizes]\r\n";
-            for (int index = 0; index < fileInfoList.Count; index++)
+            //for (int index = 0; index < fileInfoList.Count; index++)
+            //{
+            //    var fileInfo = fileInfoList[index];
+            //    sizeString += index + 1 + "=" + fileInfo.Length + "\r\n";
+            //}
+            var index = 1;
+            foreach (var picture in order.Pictures)
             {
-                var fileInfo = fileInfoList[index];
-                sizeString += index + 1 + "=" + fileInfo.Length + "\r\n";
+                var fileInfo = fileInfoList[index - 1];
+                sizeString += index + "=" + fileInfo.Length + "\r\n";
+                index++;
             }
             return sizeString;
         }
 
-        private  void GetFileInfo()
+        private string FillFitOrFill()
         {
-            var filenames = Directory.GetFiles(folderfilepath,"*.jpeg");
-            foreach (var filename in filenames)
+            var fitOrFillString = "[FitOrFill]\r\n";
+            var index = 1;
+            foreach (var picture in order.Pictures)
             {
-                var fileinfo = new FileInfo(filename);
+                fitOrFillString += index + "=" + picture.FitOrFill + "\r\n";
+                index++;
+            }
+            return fitOrFillString;
+        }
+
+        private string Fill10X15()
+        {
+            var _10X15string = "[10x15]\r\n";
+            for (var index = 0; index < order.Pictures.Count; index++)
+            {
+                if (order.Pictures[index].Size == "10x15")
+                {
+                    _10X15string += index + 1 + "=" + order.Pictures[index].Amount + "\r\n";
+
+                }
+
+            }
+            return _10X15string;
+        }
+
+        private void GetFileInfo()
+        {
+            foreach (var picture in order.Pictures)
+            {
+                var fileinfo = new FileInfo(picture.FilePath);
                 fileInfoList.Add(fileinfo);
             }
         }
